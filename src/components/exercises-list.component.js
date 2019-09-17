913d0e502ae82cb616f547cea020fae73a03dab5
runnerbrain <runnerbrain@gmail.com>
2019-09-16 22:32:36 -0400

    added exercise list component

--- src/components/exercises-list.component.js
+++ src/components/exercises-list.component.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import axios from 'axios';
+
+const Exercise = props => (
+  <tr>
+    <td>{props.exercise.username}</td>
+    <td>{props.exercise.description}</td>
+    <td>{props.exercise.duration}</td>
+    <td>{props.exercise.date}</td>
+    <td>
+      <Link to={'/edit/' + props.exercise._id}>
+        <button>edit</button>
+      </Link>
+    </td>
+  </tr>
+);
 
 class ExercisesList extends Component {
+  constructor(props) {
+    super(props);
 
-    render() {
-        return (
-            <div>
-                <p>Exercises list component</p>
-            </div>
-        );
-    }
+    this.state = {
+      exercises: []
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get('http://localhost:5000/exercises')
+      .then(res => {
+        this.setState({ exercises: res.data });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  deleteExercidse(id) {
+    axios
+      .delete(`http://localhost:5000/exercises/${id}`)
+      .then(res => console.log(res.data));
+    this.setState({
+      exercises: this.state.exercises.filter(el => el._id !== id)
+    });
+  }
+
+  exerciseList() {
+    return this.state.exercises.map(currentExcercise => (
+      <Exercise
+        exercise={currentExcercise}
+        deleteExercise={this.deleteExercidse}
+        key={currentExcercise._id}
+      />
+    ));
+  }
+
+  render() {
+    return (
+      <div>
+        <h3>Exercises List:</h3>
+        <table className="table">
+          <thead className="thead-light">
+            <tr>
+              <th>Username</th>
+              <th>Description</th>
+              <th>Duration</th>
+              <th>Date</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>{this.exerciseList()}</tbody>
+        </table>
+      </div>
+    );
+  }
 }
 
 export default ExercisesList;
